3409ab58d6300f1bd2a105a4287b41ca5caaa4ee
Saulėnas <73237821+Saulala31@users.noreply.github.com>
2024-04-17 20:02:40 +0300

    deleted

--- models/user.model.js
+++ models/user.model.js
@@ -14,10 +14,6 @@ const userSchema = new Schema({
         type: String,
         required: [true, "password is required"],
     },
-    UserID: {
-        type: String,
-        required: [true, "user is required"],
-    },
 },{timestamps:true});
 
 
